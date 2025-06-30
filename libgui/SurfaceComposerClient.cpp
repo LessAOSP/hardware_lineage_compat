LineageOS/android_hardware_lineage_compat

compat: Rework libgui shim
Change-Id: I9a1a8f40a60779d179c5c25946e10e206cbf0987

libgui/SurfaceComposerClient.cpp
@@ -4,36 +4,25 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <utils/Errors.h>
+#include "SurfaceComposerClient.h"
 
-using android::status_t;
+namespace android {
+namespace SurfaceComposerClient {
 
-extern "C" status_t _ZN7android21SurfaceComposerClient11Transaction5applyEbb(void* thisptr,
-                                                                             bool synchronous,
-                                                                             bool oneWay);
+std::string kEmpty;
 
-extern "C" void*
-_ZN7android21SurfaceComposerClient20createVirtualDisplayERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEbS9_f(
-        void* displayName, bool isSecure, void* uniqueId, float requestedRefreshRate);
-
-extern "C" void* _ZN7android21SurfaceComposerClient6kEmptyE;
-
-extern "C" void _ZN7android21SurfaceComposerClient21destroyVirtualDisplayERKNS_2spINS_7IBinderEEE(
-        void* displayToken);
-
-extern "C" status_t _ZN7android21SurfaceComposerClient11Transaction5applyEb(void* thisptr,
-                                                                            bool synchronous) {
-    return _ZN7android21SurfaceComposerClient11Transaction5applyEbb(thisptr, synchronous, false);
+sp<IBinder> createDisplay(const std::string& displayName, bool isSecure,
+                          float requestedRefreshRate) {
+    return createVirtualDisplay(displayName, isSecure, kEmpty, requestedRefreshRate);
 }
 
-extern "C" void* _ZN7android21SurfaceComposerClient13createDisplayERKNS_7String8Ebf(
-        void* displayName, bool isSecure, float requestedRefreshRate) {
-    return _ZN7android21SurfaceComposerClient20createVirtualDisplayERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEbS9_f(
-            displayName, isSecure, _ZN7android21SurfaceComposerClient6kEmptyE,
-            requestedRefreshRate);
+status_t destroyDisplay(const sp<IBinder>& displayToken) {
+    return destroyVirtualDisplay(displayToken);
 }
 
-extern "C" void _ZN7android21SurfaceComposerClient14destroyDisplayERKNS_2spINS_7IBinderEEE(
-        void* displayToken) {
-    _ZN7android21SurfaceComposerClient21destroyVirtualDisplayERKNS_2spINS_7IBinderEEE(displayToken);
+status_t Transaction::apply(bool synchronous) {
+    return apply(synchronous, false);
 }
+
+}  // namespace SurfaceComposerClient
+}  // namespace android

libgui/SurfaceComposerClient.h
@@ -1,0 +1,27 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <binder/IBinder.h>
+
+namespace android {
+namespace SurfaceComposerClient {
+
+sp<IBinder> createVirtualDisplay(const std::string& displayName, bool isSecure,
+                                 const std::string& uniqueId, float requestedRefreshRate);
+sp<IBinder> createDisplay(const std::string& displayName, bool isSecure,
+                          float requestedRefreshRate);
+
+status_t destroyVirtualDisplay(const sp<IBinder>& displayToken);
+status_t destroyDisplay(const sp<IBinder>& displayToken);
+
+class Transaction {
+  public:
+    status_t apply(bool synchronous, bool oneWay);
+    status_t apply(bool synchronous);
+};
+
+}  // namespace SurfaceComposerClient
+}  // namespace android

LessAOSP/hardware_lineage_compat

libgui/SurfaceComposerClient.cpp
@@ -1,39 +1,28 @@
 /*
- * Copyright (C) 2022-2023 The LineageOS Project
+ * Copyright (C) 2022-2025 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <utils/Errors.h>
+#include "SurfaceComposerClient.h"
 
-using android::status_t;
+namespace android {
+namespace SurfaceComposerClient {
 
-extern "C" status_t _ZN7android21SurfaceComposerClient11Transaction5applyEbb(void* thisptr,
-                                                                             bool synchronous,
-                                                                             bool oneWay);
+std::string kEmpty;
 
-extern "C" void*
-_ZN7android21SurfaceComposerClient20createVirtualDisplayERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEbS9_f(
-        void* displayName, bool isSecure, void* uniqueId, float requestedRefreshRate);
-
-extern "C" void* _ZN7android21SurfaceComposerClient6kEmptyE;
-
-extern "C" void _ZN7android21SurfaceComposerClient21destroyVirtualDisplayERKNS_2spINS_7IBinderEEE(
-        void* displayToken);
-
-extern "C" status_t _ZN7android21SurfaceComposerClient11Transaction5applyEb(void* thisptr,
-                                                                            bool synchronous) {
-    return _ZN7android21SurfaceComposerClient11Transaction5applyEbb(thisptr, synchronous, false);
+sp<IBinder> createDisplay(const android::String8& displayName, bool isSecure,
+                          float requestedRefreshRate) {
+    return createVirtualDisplay(displayName.c_str(), isSecure, kEmpty, requestedRefreshRate);
 }
 
-extern "C" void* _ZN7android21SurfaceComposerClient13createDisplayERKNS_7String8Ebf(
-        void* displayName, bool isSecure, float requestedRefreshRate) {
-    return _ZN7android21SurfaceComposerClient20createVirtualDisplayERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEbS9_f(
-            displayName, isSecure, _ZN7android21SurfaceComposerClient6kEmptyE,
-            requestedRefreshRate);
+status_t destroyDisplay(const sp<IBinder>& displayToken) {
+    return destroyVirtualDisplay(displayToken);
 }
 
-extern "C" void _ZN7android21SurfaceComposerClient14destroyDisplayERKNS_2spINS_7IBinderEEE(
-        void* displayToken) {
-    _ZN7android21SurfaceComposerClient21destroyVirtualDisplayERKNS_2spINS_7IBinderEEE(displayToken);
+status_t Transaction::apply(bool synchronous) {
+    return apply(synchronous, false);
 }
+
+}  // namespace SurfaceComposerClient
+}  // namespace android

libgui/SurfaceComposerClient.h
@@ -1,0 +1,27 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <binder/IBinder.h>
+
+namespace android {
+namespace SurfaceComposerClient {
+
+sp<IBinder> createVirtualDisplay(const std::string& displayName, bool isSecure,
+                                 const std::string& uniqueId, float requestedRefreshRate);
+sp<IBinder> createDisplay(const android::String8& displayName, bool isSecure,
+                          float requestedRefreshRate);
+
+status_t destroyVirtualDisplay(const sp<IBinder>& displayToken);
+status_t destroyDisplay(const sp<IBinder>& displayToken);
+
+class Transaction {
+  public:
+    status_t apply(bool synchronous, bool oneWay);
+    status_t apply(bool synchronous);
+};
+
+}  // namespace SurfaceComposerClient
+}  // namespace android